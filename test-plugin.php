9d2da158c8bfbf8a50b2b4f6c147bcaa1bdf9c15
Talha Quddoos <60311647+TalhaQuddoos@users.noreply.github.com>
2022-05-29 19:47:12 +0500

    Update test-plugin.php

--- test-plugin.php
+++ test-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Test Plugin
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       Handle the basics with this plugin.
- * Version:           1.25.3
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            John Smith
@@ -44,7 +44,7 @@ function talha_plugin_info( $res, $action, $args ){
 
 	// info.json is the file with the actual plugin information on your server
 	$remote = wp_remote_get( 
-		'https://rudrastyh.com/wp-content/uploads/updater/info.json', 
+		'https://raw.githubusercontent.com/TalhaQuddoos/test-wp-plugin/main/info.json', 
 		array(
 			'timeout' => 10,
 			'headers' => array(
@@ -57,7 +57,7 @@ function talha_plugin_info( $res, $action, $args ){
 	if( 
 		is_wp_error( $remote )
 		|| 200 !== wp_remote_retrieve_response_code( $remote )
-		|| empty( wp_remote_retrieve_body( $remote ) 
+		|| empty( wp_remote_retrieve_body( $remote ) )
 	) {
 		return $res;	
 	}
@@ -94,5 +94,60 @@ function talha_plugin_info( $res, $action, $args ){
 	);
 	
 	return $res;
+
+}
+
+
+
+add_filter( 'site_transient_update_plugins', 'misha_push_update' );
+ 
+function misha_push_update( $transient ){
+ 
+	if ( empty( $transient->checked ) ) {
+		return $transient;
+	}
+
+	$remote = wp_remote_get( 
+		'https://raw.githubusercontent.com/TalhaQuddoos/test-wp-plugin/main/info.json',
+		array(
+			'timeout' => 10,
+			'headers' => array(
+				'Accept' => 'application/json'
+			)
+		)
+	);
+
+	if( 
+		is_wp_error( $remote )
+		|| 200 !== wp_remote_retrieve_response_code( $remote )
+		|| empty( wp_remote_retrieve_body( $remote ) )
+	) {
+		return $transient;	
+	}
+	
+	$remote = json_decode( wp_remote_retrieve_body( $remote ) );
+ 
+		// your installed plugin version should be on the line below! You can obtain it dynamically of course 
+	if(
+		$remote
+		&& version_compare( get_plugin_data(__FILE__)['Version'], $remote->version, '<' )
+		&& version_compare( $remote->requires, get_bloginfo( 'version' ), '<' )
+		&& version_compare( $remote->requires_php, PHP_VERSION, '<' )
+	) {
+
+        
+		
+		$res = new stdClass();
+		$res->slug = $remote->slug;
+		$res->plugin = plugin_basename( __FILE__ ); // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
+		$res->new_version = $remote->version;
+		$res->tested = $remote->tested;
+		$res->package = $remote->download_url;
+		$transient->response[ $res->plugin ] = $res;
+		
+		//$transient->checked[$res->plugin] = $remote->version;
+	}
+ 
+	return $transient;
 
 }
